a778e5250859a4b76b3185765a04bfcda5dabcb0
LeonH <leonheinze011@gmail.com>
2024-11-26 14:50:55 +0200

    Update Server.cpp

--- Socket/Server/Server.cpp
+++ Socket/Server/Server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <string> // Fï¿½r std::string und std::getline
 #define BUFFER_SIZE 2048
 using namespace std;
 
@@ -8,10 +9,11 @@ using namespace std;
 
 int main() {
     WSADATA wsaData;
-    SOCKET serverSocket, clientSocket;
-    SOCKADDR_IN serverAddr, clientAddr;
+    SOCKET clientSocket;
+    SOCKADDR_IN serverAddr;
     char buffer[BUFFER_SIZE];
-    int clientAddrSize = sizeof(clientAddr);
+    bool cont = 1;
+    string message;
 
     int result = WSAStartup(MAKEWORD(2, 2), &wsaData); // Initialisiert die WinSock-Bibliothek
     if (result != 0) {
@@ -20,37 +22,31 @@ int main() {
         return 1;
     }
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0); // Erstellt einen neuen TCP-Socket
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0); // Erstellt einen neuen TCP-Socket
 
-    serverAddr.sin_addr.s_addr = INADDR_ANY; // Akzeptiert Verbindungen von jeder IP-Adresse
     serverAddr.sin_family = AF_INET; // Verwendet das IPv4-Protokoll
     serverAddr.sin_port = htons(54000); // Setzt den Port auf 54000
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); // Konvertiert die IP-Adresse von Text- in Binï¿½rform
 
-    bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)); // Bindet den Socket an die Adresse und den Port
-    listen(serverSocket, SOMAXCONN); // Wartet auf eingehende Verbindungen
+    connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)); // Verbindet sich mit dem Server
 
-    cout << "Waiting for a connection... \n";
+    cout << "Connected to the server!\n";
 
-    clientSocket = accept(serverSocket, (SOCKADDR*)&clientAddr, &clientAddrSize); // Akzeptiert eine eingehende Verbindung
-
-    cout << "Client connected! \n";
-
-    // Schleife, um Nachrichten zu empfangen und zu senden
     while (true) {
-        int bytesReceived = recv(clientSocket, buffer, 1024, 0); // Empfängt Daten vom Client
-        if (bytesReceived == SOCKET_ERROR || bytesReceived == 0) {
-            cout << "Connection closed";
-            break;
-        }
-        buffer[bytesReceived] = '\0'; // Nullterminierung des empfangenen Datenblocks
-        cout << "Received: " << buffer << "\n";
+        cout << "Enter your message: ";
+        getline(cin >> ws, message);
+        send(clientSocket, message.c_str(), message.size() + 1, 0); // Sendet eine Nachricht an den Server
+        int bytesReceived = recv(clientSocket, buffer, 1024, 0); // Empfï¿½ngt die Antwort vom Server
 
-        string response = "Message received";
-        send(clientSocket, response.c_str(), response.size() + 1, 0); // Antwortet dem Client
+        if (bytesReceived > 0) {
+            buffer[bytesReceived] = '\0'; // Nullterminierung des empfangenen Datenblocks
+            cout << "Received: " << buffer << "\n";
+        }
+        /*cout << "If you wanna go on press 1, else press 0. -> ";
+        cin >> cont;*/
     }
 
-    closesocket(clientSocket); // Schließt den Client-Socket
-    closesocket(serverSocket); // Schließt den Server-Socket
+    closesocket(clientSocket); // Schlieï¿½t den Client-Socket
     WSACleanup(); // Beendet die Verwendung der WinSock-Bibliothek
 
     return 0;
